95969e5052ec40a950efc166f30fe0376934b416
opabhijeet <abhijeet.2201164ec@iiitbh.ac.in>
2024-06-02 11:50:07 +0530

    production mode

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: './src/index.js',
-  mode: 'development',
+  mode: 'production',
   plugins: [
     new HtmlWebpackPlugin({
         template: './src/index.html',
